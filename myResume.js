eff57871a118c8d87e22b83a881c825a0daec4d6
wangtao <1621829316@qq.com>
2018-04-19 14:41:54 +0800

    添加以下功能: 会变的 topNavBar 会动的二级菜单 点击导航栏自己滚动到对应 div

--- myResume.js
+++ myResume.js
@@ -4,6 +4,11 @@ var resume = {
         this.portfolio2 = document.getElementById('portfolio2')
         this.portfolio3 = document.getElementById('portfolio3')
         this.portfolioBar = document.getElementById('portfolioBar')
+        this.topNavBar = document.querySelector('.topNavBar')
+        this.liTags = document.querySelectorAll('.topNavBar>nav>ul>li')
+        this.aTags = document.querySelectorAll('.topNavBar>nav>ul>li>a')
+        this.subMenuLiTags = document.querySelectorAll('.topNavBar .subMenu li')
+        console.log(this.aTags)
         this.bind()
     },
     bind: function() {
@@ -17,8 +22,31 @@ var resume = {
         this.portfolio3.onclick = function() {
             _this.portfolioBar.className = "bar state-3"
         }
+        window.onscroll = function() {
+            if (window.scrollY > 0) {
+                _this.topNavBar.className = "topNavBar sticky"
+            } else {
+                _this.topNavBar.className = "topNavBar"
+            }
+        }
+        this.liTags.forEach(function(element) {
+            element.onmouseenter = function() {
+                this.classList.add('active')
+            }
+            element.onmouseleave = function() {
+                this.classList.remove('active')
+            }
+        })
+        this.aTags.forEach(function(element) {
+            element.onclick = function(event) {
+                event.preventDefault();
+                var href = this.getAttribute('href') //得到#siteAbout,直接this.href会得到浏览器自动加上的http
+                var element = document.querySelector(href)
+                var elementOffsetTop = element.offsetTop
+                window.scrollTo(0, elementOffsetTop - 80)
+            }
+        })
     }
-
 }
 window.onload = function() {
     document.getElementById('site-welcome').className = "site-welcome"
